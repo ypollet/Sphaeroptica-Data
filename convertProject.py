6dbe8c125815a4086b4832bbee23f56a92d2d8d1
Yann Pollet <pollet.yann1999@gmail.com>
2025-02-03 18:26:00 +0100

    Fix : converted data

---
+++ convertProject.py
@@ -0,0 +1,49 @@
+import sys
+import json
+import math
+from photogrammetry import reconstruction, converters, helpers
+import numpy as np
+
+if(len(sys.argv) < 2):
+    print("Need a path")
+    exit(404)
+
+path = sys.argv[1]
+
+with open(path, "r") as f:
+    calib_file = json.load(f)
+
+
+intrinsics : dict = calib_file["intrinsics"]
+if "camera matrix" in intrinsics:
+    intrinsics["cameraMatrix"] = intrinsics.pop('camera matrix')
+if "distortion matrix" in intrinsics:
+    intrinsics["distortionMatrix"] = intrinsics.pop('distortion matrix')
+
+print(intrinsics.keys())
+
+intrinsics["cameraMatrix"]["matrix"] = np.array(intrinsics["cameraMatrix"]["matrix"]).reshape(-1).tolist()
+intrinsics["distortionMatrix"]["matrix"] = np.array(intrinsics["distortionMatrix"]["matrix"]).reshape(-1).tolist()
+
+for image_name in calib_file["extrinsics"]:
+    calib_file["extrinsics"][image_name]["matrix"] = np.array(calib_file["extrinsics"][image_name]["matrix"]).reshape(-1).tolist()
+
+done = False
+while not done:
+    ret = input("Save it to same file ? (yes/no/cancel) : ")
+    match ret:
+        case "yes":
+            done = True
+            break
+        case "no":
+            path = input("Path of where to save : ")
+            done = True
+            break
+        case "cancel":
+            exit(0)
+        case _:
+            done = False
+
+with open(path, "w") as f:
+    json.dump(calib_file, f)
+
